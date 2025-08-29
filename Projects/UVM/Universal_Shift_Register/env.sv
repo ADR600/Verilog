b016eaa0888820d6bc1cf3d9b1cc2a1f25d8ecf1
Ankith Rao <ankithgaming@gmail.com>
2025-08-29 23:09:11 +0530

    Update env.sv

--- Projects/UVM/Universal_Shift_Register/env.sv
+++ Projects/UVM/Universal_Shift_Register/env.sv
@@ -3,6 +3,8 @@ class env extends uvm_env;
   write_agent wagt;
   read_agent ragt;
   scoreboard sb;
+  virtual_sequencer vseqr;
+  
   function new (string name = "env", uvm_component parent);
     super.new(name,parent);
   endfunction 
@@ -10,9 +12,11 @@ class env extends uvm_env;
     wagt = write_agent :: type_id :: create ("wagt",this);
     ragt = read_agent :: type_id :: create ("ragt",this);
     sb = scoreboard :: type_id :: create ("sb",this);
+    vseqr = virtual_sequencer:: type_id :: create ("vseqr",this);
   endfunction 
   function void connect_phase (uvm_phase phase);
     wagt.mon.monitor_port.connect(sb.wfifo.analysis_export);
     ragt.mon.monitor_port.connect(sb.rfifo.analysis_export);
+    vseqr.seqr = wagt.seqr;
   endfunction 
 endclass 
